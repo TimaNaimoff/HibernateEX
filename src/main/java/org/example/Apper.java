c2017e2fcde36ead923ebeb9fe53797b46a77a83
Temur Vegas <timanaimoff@gmail.com>
2023-01-25 15:23:07 +0500

    Task is completed!

--- src/main/java/org/example/Apper.java
+++ src/main/java/org/example/Apper.java
@@ -6,6 +6,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Apper {
@@ -27,12 +28,28 @@ public class Apper {
 //                  Director directoris=actor1.getTheDirector();
 //                  System.out.println(directoris);
 //                  System.out.println("-----------------------------");
-                    Director director=session.get(Director.class,1);
-                    Actor actor=new Actor("Sheppard",154);
-                    director.getActors().add(actor);
-                    actor.setTheDirector(director);
-                    session.save(actor);
-                  session.getTransaction().commit();
+//                    Director director2=session.get(Director.class,1);
+//                    Actor actor2=new Actor("Sheppard",154);
+//                    director2.getActors().add(actor2);
+//                    actor2.setTheDirector(director2);
+//                    session.save(actor2);
+//                Actor actor3=new Actor("Natali",21);
+//                Director director3=new Director("Taynan",54);
+//                actor3.setTheDirector(director3);
+//                director3.setActors(Collections.singletonList(actor3));
+//                session.save(director3);
+//                session.save(actor3);
+//                Actor actor4 = session.get(Actor.class,4);
+//                Director director4=session.get(Director.class,2);
+//                actor4.getTheDirector().getActors().remove(actor4);
+//                actor4.setTheDirector(director4);
+//                director4.getActors().add(actor4);
+                Director director = session.get(Director.class,1);
+                List<Actor>actors=director.getActors();
+                for(Actor ac:actors){
+                    session.remove(ac);
+                }
+                session.getTransaction().commit();
             }catch(Exception e){
                 session.getTransaction().rollback();
             }
